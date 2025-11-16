37cd2070d0ae81ab86abdf985fd9d935324160f6
Jeongmin Cho <jojm0829@gmail.com>
2025-11-16 20:38:08 +0900

    ui: 404 not found 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <div className="w-full min-h-[calc(100vh-60px)] flex flex-col justify-center items-center">
+      <div className="text-lg">페이지를 찾을 수 없습니다.</div>
+    </div>
+  );
+}
