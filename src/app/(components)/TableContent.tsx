8a58136509820e32068d1adc3256d54fa3cd269d
Jeongmin Cho <jojm0829@gmail.com>
2025-11-16 23:09:59 +0900

    ui: 페이지네이션 가운데 정렬

--- src/app/(components)/TableContent.tsx
+++ src/app/(components)/TableContent.tsx
@@ -34,90 +34,92 @@ export default function TableContent({
 
   return (
     <div>
-      <table className="w-full rounded-lg outline outline-gray-300 overflow-hidden">
-        <colgroup>
-          <col className="w-14" />
-          <col className="w-auto" />
-          <col className="w-60" />
-          <col className="w-10" />
-        </colgroup>
-        <thead>
-          <tr className="h-12 bg-gray-100">
-            <th>
-              <Checkbox
-                checked={isMasterChecked}
-                onClick={() => onClickMaster(items?.map((item) => item.id))}
-                className="ml-5"
-              />
-            </th>
-            <th className="text-left">회사명</th>
-            <th className="text-left">생성일자</th>
-            <th className="text-left"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {items?.length === 0 ? (
-            <tr className="h-40">
-              <td colSpan={4} className="text-center">
-                관심기업이 없습니다.
-              </td>
+      <div className="flex flex-col items-center">
+        <table className="w-full rounded-lg outline outline-gray-300 overflow-hidden">
+          <colgroup>
+            <col className="w-14" />
+            <col className="w-auto" />
+            <col className="w-60" />
+            <col className="w-10" />
+          </colgroup>
+          <thead>
+            <tr className="h-12 bg-gray-100">
+              <th>
+                <Checkbox
+                  checked={isMasterChecked}
+                  onClick={() => onClickMaster(items?.map((item) => item.id))}
+                  className="ml-5"
+                />
+              </th>
+              <th className="text-left">회사명</th>
+              <th className="text-left">생성일자</th>
+              <th className="text-left"></th>
             </tr>
-          ) : (
-            items?.map((item: Item) => (
-              <tr
-                className={clsx(
-                  "border-t border-t-gray-300 h-12",
-                  checkedList.includes(item.id)
-                    ? "bg-primary-100 hover:bg-primary-200"
-                    : "hover:bg-gray-50"
-                )}
-                key={item.id}
-              >
-                <td className="cursor-default">
-                  <Checkbox
-                    checked={checkedList.includes(item.id)}
-                    onClick={() => {
-                      toggleCheck(item.id, items.length);
-                    }}
-                    className="ml-5"
-                  />
-                </td>
-                <td
-                  onClick={() => {
-                    onOpenCloseItem(item.id);
-                  }}
-                  className="cursor-pointer"
-                >
-                  {item.company_name}
-                </td>
-                <td
-                  onClick={() => {
-                    onOpenCloseItem(item.id);
-                  }}
-                  className="cursor-pointer"
-                >
-                  {item.created_at}
-                </td>
-                <td className="">
-                  <Trash
-                    onClick={() => {
-                      setSingleDeleteId(item.id);
-                      onOpenDelete();
-                    }}
-                    size={20}
-                    className="text-border cursor-pointer"
-                  />
+          </thead>
+          <tbody>
+            {items?.length === 0 ? (
+              <tr className="h-40">
+                <td colSpan={4} className="text-center">
+                  관심기업이 없습니다.
                 </td>
               </tr>
-            ))
-          )}
-        </tbody>
-      </table>
-      <Pagination
-        page={page}
-        totalPages={favorites?.total_pages || 1}
-        onChange={onChangePage}
-      />
+            ) : (
+              items?.map((item: Item) => (
+                <tr
+                  className={clsx(
+                    "border-t border-t-gray-300 h-12",
+                    checkedList.includes(item.id)
+                      ? "bg-primary-100 hover:bg-primary-200"
+                      : "hover:bg-gray-50"
+                  )}
+                  key={item.id}
+                >
+                  <td className="cursor-default">
+                    <Checkbox
+                      checked={checkedList.includes(item.id)}
+                      onClick={() => {
+                        toggleCheck(item.id, items.length);
+                      }}
+                      className="ml-5"
+                    />
+                  </td>
+                  <td
+                    onClick={() => {
+                      onOpenCloseItem(item.id);
+                    }}
+                    className="cursor-pointer"
+                  >
+                    {item.company_name}
+                  </td>
+                  <td
+                    onClick={() => {
+                      onOpenCloseItem(item.id);
+                    }}
+                    className="cursor-pointer"
+                  >
+                    {item.created_at}
+                  </td>
+                  <td className="">
+                    <Trash
+                      onClick={() => {
+                        setSingleDeleteId(item.id);
+                        onOpenDelete();
+                      }}
+                      size={20}
+                      className="text-border cursor-pointer"
+                    />
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+        <Pagination
+          page={page}
+          totalPages={favorites?.total_pages || 1}
+          onChange={onChangePage}
+        />
+      </div>
       <SlidePanel
         open={slidePanelOpen}
         onClose={() => onOpenCloseItem(undefined)}
